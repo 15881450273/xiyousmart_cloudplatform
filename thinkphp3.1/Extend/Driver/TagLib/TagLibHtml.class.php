a2f8ea55f4a58d277b92cd7de97a8476abf402bd
zwj <958654064@qq.com>
2019-11-12 16:54:52 +0800

    第三版本培训平台（考核测评、每日一测） Signed-off-by: zwj <958654064@qq.com>

--- thinkphp3.1/Extend/Driver/TagLib/TagLibHtml.class.php
+++ thinkphp3.1/Extend/Driver/TagLib/TagLibHtml.class.php
@@ -175,6 +175,356 @@ class TagLibHtml extends TagLib{
         }
         $parseStr   .= '</select>';
         return $parseStr;
-    
+    }
+
+    /**
+     * checkbox标签解析
+     * 格式： <html:checkbox checkboxes="" checked="" />
+     * @access public
+     * @param string $attr 标签属性
+     * @return string|void
+     */
+    public function _checkbox($attr) {
+        $tag        = $this->parseXmlAttr($attr,'checkbox');
+        $name       = $tag['name'];
+        $checkboxes = $tag['checkboxes'];
+        $checked    = $tag['checked'];
+        $separator  = $tag['separator'];
+        $checkboxes = $this->tpl->get($checkboxes);
+        $checked    = $this->tpl->get($checked)?$this->tpl->get($checked):$checked;
+        $parseStr   = '';
+        foreach($checkboxes as $key=>$val) {
+            if($checked == $key  || in_array($key,$checked) ) {
+                $parseStr .= '<input type="checkbox" checked="checked" name="'.$name.'[]" value="'.$key.'">'.$val.$separator;
+            }else {
+                $parseStr .= '<input type="checkbox" name="'.$name.'[]" value="'.$key.'">'.$val.$separator;
+            }
+        }
+        return $parseStr;
+    }
+
+    /**
+     * radio标签解析
+     * 格式： <html:radio radios="name" checked="value" />
+     * @access public
+     * @param string $attr 标签属性
+     * @return string|void
+     */
+    public function _radio($attr) {
+        $tag        = $this->parseXmlAttr($attr,'radio');
+        $name       = $tag['name'];
+        $radios     = $tag['radios'];
+        $checked    = $tag['checked'];
+        $separator  = $tag['separator'];
+        $radios     = $this->tpl->get($radios);
+        $checked    = $this->tpl->get($checked)?$this->tpl->get($checked):$checked;
+        $parseStr   = '';
+        foreach($radios as $key=>$val) {
+            if($checked == $key ) {
+                $parseStr .= '<input type="radio" checked="checked" name="'.$name.'[]" value="'.$key.'">'.$val.$separator;
+            }else {
+                $parseStr .= '<input type="radio" name="'.$name.'[]" value="'.$key.'">'.$val.$separator;
+            }
+
+        }
+        return $parseStr;
+    }
+
+    /**
+     * list标签解析
+     * 格式： <html:grid datasource="" show="vo" />
+     * @access public
+     * @param string $attr 标签属性
+     * @return string
+     */
+    public function _grid($attr) {
+        $tag        = $this->parseXmlAttr($attr,'grid');
+        $id         = $tag['id'];                       //表格ID
+        $datasource = $tag['datasource'];               //列表显示的数据源VoList名称
+        $pk         = empty($tag['pk'])?'id':$tag['pk'];//主键名，默认为id
+        $style      = $tag['style'];                    //样式名
+        $name       = !empty($tag['name'])?$tag['name']:'vo';                 //Vo对象名
+        $action     = !empty($tag['action'])?$tag['action']:false;                   //是否显示功能操作
+        $key         =  !empty($tag['key'])?true:false;
+        if(isset($tag['actionlist'])) {
+            $actionlist = explode(',',trim($tag['actionlist']));    //指定功能列表
+        }
+
+        if(substr($tag['show'],0,1)=='$') {
+            $show   = $this->tpl->get(substr($tag['show'],1));
+        }else {
+            $show   = $tag['show'];
+        }
+        $show       = explode(',',$show);                //列表显示字段列表
+
+        //计算表格的列数
+        $colNum     = count($show);
+        if(!empty($action))     $colNum++;
+        if(!empty($key))  $colNum++;
+
+        //显示开始
+		$parseStr	= "<!-- Think 系统列表组件开始 -->\n";
+        $parseStr  .= '<table id="'.$id.'" class="'.$style.'" cellpadding=0 cellspacing=0 >';
+        $parseStr  .= '<tr><td height="5" colspan="'.$colNum.'" class="topTd" ></td></tr>';
+        $parseStr  .= '<tr class="row" >';
+        //列表需要显示的字段
+        $fields = array();
+        foreach($show as $val) {
+        	$fields[] = explode(':',$val);
+        }
+
+        if(!empty($key)) {
+            $parseStr .= '<th width="12">No</th>';
+        }
+        foreach($fields as $field) {//显示指定的字段
+            $property = explode('|',$field[0]);
+            $showname = explode('|',$field[1]);
+            if(isset($showname[1])) {
+                $parseStr .= '<th width="'.$showname[1].'">';
+            }else {
+                $parseStr .= '<th>';
+            }
+            $parseStr .= $showname[0].'</th>';
+        }
+        if(!empty($action)) {//如果指定显示操作功能列
+            $parseStr .= '<th >操作</th>';
+        }
+        $parseStr .= '</tr>';
+        $parseStr .= '<volist name="'.$datasource.'" id="'.$name.'" ><tr class="row" >';	//支持鼠标移动单元行颜色变化 具体方法在js中定义
+
+        if(!empty($key)) {
+            $parseStr .= '<td>{$i}</td>';
+        }
+        foreach($fields as $field) {
+            //显示定义的列表字段
+            $parseStr   .=  '<td>';
+            if(!empty($field[2])) {
+                // 支持列表字段链接功能 具体方法由JS函数实现
+                $href = explode('|',$field[2]);
+                if(count($href)>1) {
+                    //指定链接传的字段值
+                    // 支持多个字段传递
+                    $array = explode('^',$href[1]);
+                    if(count($array)>1) {
+                        foreach ($array as $a){
+                            $temp[] =  '\'{$'.$name.'.'.$a.'|addslashes}\'';
+                        }
+                        $parseStr .= '<a href="javascript:'.$href[0].'('.implode(',',$temp).')">';
+                    }else{
+                        $parseStr .= '<a href="javascript:'.$href[0].'(\'{$'.$name.'.'.$href[1].'|addslashes}\')">';
+                    }
+                }else {
+                    //如果没有指定默认传编号值
+                    $parseStr .= '<a href="javascript:'.$field[2].'(\'{$'.$name.'.'.$pk.'|addslashes}\')">';
+                }
+            }
+            if(strpos($field[0],'^')) {
+                $property = explode('^',$field[0]);
+                foreach ($property as $p){
+                    $unit = explode('|',$p);
+                    if(count($unit)>1) {
+                        $parseStr .= '{$'.$name.'.'.$unit[0].'|'.$unit[1].'} ';
+                    }else {
+                        $parseStr .= '{$'.$name.'.'.$p.'} ';
+                    }
+                }
+            }else{
+                $property = explode('|',$field[0]);
+                if(count($property)>1) {
+                    $parseStr .= '{$'.$name.'.'.$property[0].'|'.$property[1].'}';
+                }else {
+                    $parseStr .= '{$'.$name.'.'.$field[0].'}';
+                }
+            }
+            if(!empty($field[2])) {
+                $parseStr .= '</a>';
+            }
+            $parseStr .= '</td>';
+
+        }
+        if(!empty($action)) {//显示功能操作
+            if(!empty($actionlist[0])) {//显示指定的功能项
+                $parseStr .= '<td>';
+                foreach($actionlist as $val) {
+					if(strpos($val,':')) {
+						$a = explode(':',$val);
+						if(count($a)>2) {
+                            $parseStr .= '<a href="javascript:'.$a[0].'(\'{$'.$name.'.'.$a[2].'}\')">'.$a[1].'</a>&nbsp;';
+						}else {
+							$parseStr .= '<a href="javascript:'.$a[0].'(\'{$'.$name.'.'.$pk.'}\')">'.$a[1].'</a>&nbsp;';
+						}
+					}else{
+						$array	=	explode('|',$val);
+						if(count($array)>2) {
+							$parseStr	.= ' <a href="javascript:'.$array[1].'(\'{$'.$name.'.'.$array[0].'}\')">'.$array[2].'</a>&nbsp;';
+						}else{
+							$parseStr .= ' {$'.$name.'.'.$val.'}&nbsp;';
+						}
+					}
+                }
+                $parseStr .= '</td>';
+            }
+        }
+        $parseStr	.= '</tr></volist><tr><td height="5" colspan="'.$colNum.'" class="bottomTd"></td></tr></table>';
+        $parseStr	.= "\n<!-- Think 系统列表组件结束 -->\n";
+        return $parseStr;
+    }
+
+    /**
+     * list标签解析
+     * 格式： <html:list datasource="" show="" />
+     * @access public
+     * @param string $attr 标签属性
+     * @return string
+     */
+    public function _list($attr) {
+        $tag        = $this->parseXmlAttr($attr,'list');
+        $id         = $tag['id'];                       //表格ID
+        $datasource = $tag['datasource'];               //列表显示的数据源VoList名称
+        $pk         = empty($tag['pk'])?'id':$tag['pk'];//主键名，默认为id
+        $style      = $tag['style'];                    //样式名
+        $name       = !empty($tag['name'])?$tag['name']:'vo';                 //Vo对象名
+        $action     = $tag['action']=='true'?true:false;                   //是否显示功能操作
+        $key         =  !empty($tag['key'])?true:false;
+        $sort      = $tag['sort']=='false'?false:true;
+        $checkbox   = $tag['checkbox'];                 //是否显示Checkbox
+        if(isset($tag['actionlist'])) {
+            $actionlist = explode(',',trim($tag['actionlist']));    //指定功能列表
+        }
+
+        if(substr($tag['show'],0,1)=='$') {
+            $show   = $this->tpl->get(substr($tag['show'],1));
+        }else {
+            $show   = $tag['show'];
+        }
+        $show       = explode(',',$show);                //列表显示字段列表
+
+        //计算表格的列数
+        $colNum     = count($show);
+        if(!empty($checkbox))   $colNum++;
+        if(!empty($action))     $colNum++;
+        if(!empty($key))  $colNum++;
+
+        //显示开始
+		$parseStr	= "<!-- Think 系统列表组件开始 -->\n";
+        $parseStr  .= '<table id="'.$id.'" class="'.$style.'" cellpadding=0 cellspacing=0 >';
+        $parseStr  .= '<tr><td height="5" colspan="'.$colNum.'" class="topTd" ></td></tr>';
+        $parseStr  .= '<tr class="row" >';
+        //列表需要显示的字段
+        $fields = array();
+        foreach($show as $val) {
+        	$fields[] = explode(':',$val);
+        }
+        if(!empty($checkbox) && 'true'==strtolower($checkbox)) {//如果指定需要显示checkbox列
+            $parseStr .='<th width="8"><input type="checkbox" id="check" onclick="CheckAll(\''.$id.'\')"></th>';
+        }
+        if(!empty($key)) {
+            $parseStr .= '<th width="12">No</th>';
+        }
+        foreach($fields as $field) {//显示指定的字段
+            $property = explode('|',$field[0]);
+            $showname = explode('|',$field[1]);
+            if(isset($showname[1])) {
+                $parseStr .= '<th width="'.$showname[1].'">';
+            }else {
+                $parseStr .= '<th>';
+            }
+            $showname[2] = isset($showname[2])?$showname[2]:$showname[0];
+            if($sort) {
+                $parseStr .= '<a href="javascript:sortBy(\''.$property[0].'\',\'{$sort}\',\''.ACTION_NAME.'\')" title="按照'.$showname[2].'{$sortType} ">'.$showname[0].'<eq name="order" value="'.$property[0].'" ><img src="../Public/images/{$sortImg}.gif" width="12" height="17" border="0" align="absmiddle"></eq></a></th>';
+            }else{
+                $parseStr .= $showname[0].'</th>';
+            }
+
+        }
+        if(!empty($action)) {//如果指定显示操作功能列
+            $parseStr .= '<th >操作</th>';
+        }
+
+        $parseStr .= '</tr>';
+        $parseStr .= '<volist name="'.$datasource.'" id="'.$name.'" ><tr class="row" ';	//支持鼠标移动单元行颜色变化 具体方法在js中定义
+        if(!empty($checkbox)) {
+            $parseStr .= 'onmouseover="over(event)" onmouseout="out(event)" onclick="change(event)" ';
+        }
+        $parseStr .= '>';
+        if(!empty($checkbox)) {//如果需要显示checkbox 则在每行开头显示checkbox
+            $parseStr .= '<td><input type="checkbox" name="key"	value="{$'.$name.'.'.$pk.'}"></td>';
+        }
+        if(!empty($key)) {
+            $parseStr .= '<td>{$i}</td>';
+        }
+        foreach($fields as $field) {
+            //显示定义的列表字段
+            $parseStr   .=  '<td>';
+            if(!empty($field[2])) {
+                // 支持列表字段链接功能 具体方法由JS函数实现
+                $href = explode('|',$field[2]);
+                if(count($href)>1) {
+                    //指定链接传的字段值
+                    // 支持多个字段传递
+                    $array = explode('^',$href[1]);
+                    if(count($array)>1) {
+                        foreach ($array as $a){
+                            $temp[] =  '\'{$'.$name.'.'.$a.'|addslashes}\'';
+                        }
+                        $parseStr .= '<a href="javascript:'.$href[0].'('.implode(',',$temp).')">';
+                    }else{
+                        $parseStr .= '<a href="javascript:'.$href[0].'(\'{$'.$name.'.'.$href[1].'|addslashes}\')">';
+                    }
+                }else {
+                    //如果没有指定默认传编号值
+                    $parseStr .= '<a href="javascript:'.$field[2].'(\'{$'.$name.'.'.$pk.'|addslashes}\')">';
+                }
+            }
+            if(strpos($field[0],'^')) {
+                $property = explode('^',$field[0]);
+                foreach ($property as $p){
+                    $unit = explode('|',$p);
+                    if(count($unit)>1) {
+                        $parseStr .= '{$'.$name.'.'.$unit[0].'|'.$unit[1].'} ';
+                    }else {
+                        $parseStr .= '{$'.$name.'.'.$p.'} ';
+                    }
+                }
+            }else{
+                $property = explode('|',$field[0]);
+                if(count($property)>1) {
+                    $parseStr .= '{$'.$name.'.'.$property[0].'|'.$property[1].'}';
+                }else {
+                    $parseStr .= '{$'.$name.'.'.$field[0].'}';
+                }
+            }
+            if(!empty($field[2])) {
+                $parseStr .= '</a>';
+            }
+            $parseStr .= '</td>';
+
+        }
+        if(!empty($action)) {//显示功能操作
+            if(!empty($actionlist[0])) {//显示指定的功能项
+                $parseStr .= '<td>';
+                foreach($actionlist as $val) {
+					if(strpos($val,':')) {
+						$a = explode(':',$val);
+						if(count($a)>2) {
+                            $parseStr .= '<a href="javascript:'.$a[0].'(\'{$'.$name.'.'.$a[2].'}\')">'.$a[1].'</a>&nbsp;';
+						}else {
+							$parseStr .= '<a href="javascript:'.$a[0].'(\'{$'.$name.'.'.$pk.'}\')">'.$a[1].'</a>&nbsp;';
+						}
+					}else{
+						$array	=	explode('|',$val);
+						if(count($array)>2) {
+							$parseStr	.= ' <a href="javascript:'.$array[1].'(\'{$'.$name.'.'.$array[0].'}\')">'.$array[2].'</a>&nbsp;';
+						}else{
+							$parseStr .= ' {$'.$name.'.'.$val.'}&nbsp;';
+						}
+					}
+                }
+                $parseStr .= '</td>';
+            }
+        }
+        $parseStr	.= '</tr></volist><tr><td height="5" colspan="'.$colNum.'" class="bottomTd"></td></tr></table>';
+        $parseStr	.= "\n<!-- Think 系统列表组件结束 -->\n";
+        return $parseStr;
     }
 }
